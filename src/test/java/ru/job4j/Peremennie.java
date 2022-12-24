7d448055d62f02cf071433fe6b612684318a20a7
NickGitpack <dmitriev1616@gmail.com>
2022-12-24 21:24:59 +0300

    Переменные. [#172635 #374406]

---
+++ src/test/java/ru/job4j/Peremennie.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class Peremennie {
+    public static void main(String[] args) {
+    /*
+    - возраст (byte);
+    - пол (char);
+    - рост (float);
+    - количество кирпичей для постройки дома (short);
+    - количество бактерий (double);
+    - расстояние до космических объектов (double);
+    - разрешение на работу (boolean);
+    - размер файла (long).
+     */
+        byte age = 27;
+        char gender = 'М';
+        float high = 1.9F;
+        short house = 15000;
+        double bacteria = 10000000.99;
+        double cosmos = 384000;
+        boolean wpermit = true;
+        long filesize = 1024;
+
+    }
+}
