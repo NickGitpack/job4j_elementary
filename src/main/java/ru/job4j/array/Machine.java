ac421f0b1f5a567693a6d7e00474d097da0e33e5
NickGitpack <dmitriev1616@gmail.com>
2023-01-08 12:08:09 +0300

    6.8.1. Жадный алгоритм. Сдача в кофе машине [#34741 #378531]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -7,11 +7,11 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        for (int i = 0; i < coins.length; i++) {
-            while (money - price >= coins[i]) {
-                rsl[size] = coins[i];
+        for (int i : coins) {
+            while (money - price >= i) {
+                rsl[size] = i;
                 size++;
-                price += coins[i];
+                price += i;
             }
         }
         return Arrays.copyOf(rsl, size);
